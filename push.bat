e27763ca853e121eb98d0a245ae5f962291ff80d
jppal-lang <jp.palczewski@gmail.com>
2026-08-20 11:11:29 +0200

    v4: czysty deploy bez km i czasow dojazdu

---
+++ push.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d D:\PROGRAFIT\questini.com\przewodnik-full-repo\przewodnik
+git add -A
+git commit -m "v4: czysty deploy bez km i czasow dojazdu"
+git push
+echo.
+echo === GOTOWE ===
+pause
